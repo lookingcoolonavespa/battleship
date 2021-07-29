69b0b01b0b605bc6639eda4dcb6b389043aee768
alex <alexlhliang@gmail.com>
2021-07-28 18:43:20 -0700

    working on placing ships on gameboard div

--- src/commandCenter.js
+++ src/commandCenter.js
@@ -8,53 +8,99 @@ const commandCenter = (() => {
 
   const direction = 'y';
 
-  placeShip('carrier', 5, map);
-  function placeShip(name, length, gameboard) {
+  const shipList = [];
+  createShipListItem('carrier', 5);
+  createShipListItem('battleship', 4);
+  createShipListItem('cruiser', 3);
+  createShipListItem('submarine', 3);
+  createShipListItem('patrol boat', 2);
+  console.log(shipList);
+
+  function createShipListItem(name, length) {
+    const listObj = {};
+    listObj.name = name;
+    listObj.length = length;
+    shipList.push(listObj);
+  }
+  let i = 0;
+  placeShipSeq(shipList[i].name, shipList[i].length, map);
+
+  function placeShipSeq(name, length, gameboard) {
     shipNameEl.textContent = name;
     const gridBoxes = [...gameboard.querySelectorAll('.grid-box')];
     gridBoxes.forEach((gridBox) => {
       gridBox.onmouseover = (e) => shipline(e, 'show');
       gridBox.onmouseleave = (e) => shipline(e, 'hide');
+      gridBox.onclick = (e) => placeShip(e);
     });
 
     function shipline(e, visibility) {
-      let shiplineStart = gridBoxes.findIndex(
+      const shiplineStart = gridBoxes.findIndex(
         (gridBox) => gridBox === e.target
       );
+
+      const shiplineCoords = figureOutShipline(shiplineStart);
+
+      shiplineCoords.forEach((coord) => {
+        visibility === 'show'
+          ? gridBoxes[coord].classList.add('grid-box-shipline')
+          : gridBoxes[coord].classList.remove('grid-box-shipline');
+      });
+    }
+    function placeShip(e) {
+      const shiplineStart = gridBoxes.findIndex(
+        (gridBox) => gridBox === e.target
+      );
+      const shiplineCoords = figureOutShipline(shiplineStart);
+
+      shiplineCoords.forEach((coord) =>
+        gridBoxes[coord].classList.add('grid-box-ship')
+      );
+
+      i++;
+      if (i < shipList.length) {
+        placeShipSeq(shipList[i].name, shipList[i].length, map);
+      }
+    }
+
+    function figureOutShipline(startIndex) {
+      let shiplineStart = startIndex;
+      const shiplineCoords = [];
+
       if (direction === 'y') {
         const howCloseStartIsFromColumnEnd =
           (playerGameboard.size ** 2 - // need to figure out the index of the bottom most gridBox in that column
             playerGameboard.size +
-            (shiplineStart % 8) -
-            (shiplineStart + (length - 1) * playerGameboard.size)) / // this calculates the end of the shipline
+            (shiplineStart % playerGameboard.size) -
+            (shiplineStart + (length - 1) * playerGameboard.size)) / // this calculates the index of the shipline end
           playerGameboard.size; // need to divide by column length to see how many rows left to get to the bottom
+
         if (howCloseStartIsFromColumnEnd < 0)
-          shiplineStart += howCloseStartIsFromColumnEnd * 8;
-        const shiplineCoords = [shiplineStart];
+          shiplineStart += howCloseStartIsFromColumnEnd * playerGameboard.size;
+
+        shiplineCoords.push(shiplineStart);
         for (let i = 0; i < length - 1; i++) {
           const shipCoord = shiplineCoords[i] + playerGameboard.size;
           shiplineCoords.push(shipCoord);
         }
-        shiplineCoords.forEach((coord) => {
-          visibility === 'show'
-            ? gridBoxes[coord].classList.add('grid-box-shipline')
-            : gridBoxes[coord].classList.remove('grid-box-shipline');
-        });
       }
-      if (direction === 'x') {
-        const howCloseStartIsFromRowEnd = shiplineStart % 8;
-        if ((howCloseStartIsFromRowEnd + length) % 8 < length) {
-          shiplineStart -= (howCloseStartIsFromRowEnd + length) % 8;
-        }
-        console.log(shiplineStart);
-        const shiplineEnd = shiplineStart + length;
 
+      if (direction === 'x') {
+        const howCloseStartIsFromRowEnd = shiplineStart % playerGameboard.size;
+        if (
+          (howCloseStartIsFromRowEnd + length) % playerGameboard.size <
+          length
+        ) {
+          shiplineStart -=
+            (howCloseStartIsFromRowEnd + length) % playerGameboard.size;
+        }
+
+        const shiplineEnd = shiplineStart + length;
         for (let i = shiplineStart; i < shiplineEnd; i++) {
-          visibility === 'show'
-            ? gridBoxes[i].classList.add('grid-box-shipline')
-            : gridBoxes[i].classList.remove('grid-box-shipline');
+          shiplineCoords.push(i);
         }
       }
+      return shiplineCoords;
     }
   }
 })();
